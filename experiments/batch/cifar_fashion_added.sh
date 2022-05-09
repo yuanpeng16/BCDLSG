19a855fa4272bba9c71c238bf07639978c95b78a
Yuanpeng Li <yuanpeng16@hotmail.com>
2022-05-09 19:21:33 +0800

    sleep

--- experiments/batch/cifar_fashion_added.sh
+++ experiments/batch/cifar_fashion_added.sh
@@ -2,38 +2,33 @@ ID=$(basename "$0" | sed "s/.sh$//g")
 ABS_PATH=$(readlink -f $0)
 cd $(dirname $(dirname $(dirname ${ABS_PATH})))
 
-sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 1
-sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 1
-sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 1
-sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 1
-sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 1
-sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 1
-sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 1
-sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 2
-sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 2
-sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 2
-sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 2
-sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 2
-sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 2
-sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 2
-sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 3
-sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 3
-sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 3
-sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 3
-sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 3
-sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 3
-sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 3
-sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 4
-sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 4
-sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 4
-sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 4
-sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 4
-sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 4
-sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 4
-sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 5
-sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 5
-sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 5
-sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 5
-sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 5
-sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 5
-sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 5
+sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 1; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 1; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 2; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 2; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 2; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 2; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 2; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 2; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 2; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 3; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 3; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 3; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 3; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 3; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 3; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 3; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 4; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 4; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 4; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 4; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 4; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 4; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 4; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_0_6.sh 5; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_1_5.sh 5; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_2_4.sh 5; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_3_3.sh 5; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_4_2.sh 5; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_5_1.sh 5; sleep 300
+sh experiments/cifar_fashion_added/cifar_fashion_added_6_0.sh 5; sleep 300
