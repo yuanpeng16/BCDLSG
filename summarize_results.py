f3ed37a02dd075734de786b37135bde60ac98c85
Yuanpeng Li <yuanpeng16@hotmail.com>
2022-07-01 16:11:21 +0800

    Summarize results.

--- summarize_results.py
+++ summarize_results.py
@@ -31,7 +31,7 @@ def load(fn):
     return first, last
 
 
-def format(matrix):
+def format_print(matrix):
     for line in matrix:
         print(' & '.join(line) + ' \\\\')
 
@@ -47,31 +47,61 @@ def output_result(matrix, type_names, exp_names):
             element = mean + ' {\\small$\\pm$ ' + std + '\\par}'
             line.append(element)
         ret.append(line)
-    format(ret)
+    format_print(ret)
+
+
+def dump(matrix, exp_names):
+    type_names = ['Individual', 'Shared']
+    exp_names = ['\\multicolumn{1}{c}{' + x + '}' for x in exp_names]
+    matrix = np.transpose(matrix, [2, 1, 0, 3])
+    for i in range(len(matrix)):
+        output_result(matrix[i], type_names, exp_names)
+        print()
+
+
+def dump_vertical(matrix, exp_names):
+    matrix = np.transpose(matrix, [0, 2, 1, 3])
+    assert len(matrix) == len(exp_names)
+    ret = []
+    for name, experiments in zip(exp_names, matrix):
+        line = [name]
+        for i, test in enumerate(experiments):
+            for term in test:
+                mean = str(round(term[0], 1))
+                std = str(round(term[1], 1))
+                if i == len(experiments) - 1 and len(std) < 4:
+                    element = mean + ' {\\small$\\pm$ \\ \\ ' + std + '\\par}'
+                else:
+                    element = mean + ' {\\small$\\pm$ ' + std + '\\par}'
+                line.append(element)
+        ret.append(line)
+    format_print(ret)
 
 
 def main():
-    names = [
-        'fashion_mnist_added_diagonal_acc',
-        'cifar_fashion_added_diagonal_acc',
-        'cifar_fashion_added_diagonal_resnet_acc',
-        'cifar_fashion_added_diagonal_vit_acc',
-        'reuters_reuters_diagonal_lstm_acc',
-        'reuters_reuters_diagonal_transformer_acc'
+    all_names = [
+        ['DNN', 'fashion_mnist_added_diagonal_acc'],
+        ['CNN', 'cifar_fashion_added_diagonal_acc'],
+        ['ResNet', 'cifar_fashion_added_diagonal_resnet_acc'],
+        ['ViT', 'cifar_fashion_added_diagonal_vit_acc'],
+        ['LSTM', 'reuters_reuters_diagonal_lstm_acc'],
+        ['Transformer', 'reuters_reuters_diagonal_transformer_acc'],
+        ['hLSTM', 'reuters_reuters_diagonal_lstm_horizontal_acc'],
+        ['DNN', 'fashion_fashion_added_diagonal_dnn_acc'],
+        ['CNN', 'fashion_fashion_added_diagonal_cnn_acc'],
+        ['Tile', 'fashion_mnist_added_tile_acc'],
+        ['Oneshot', 'fashion_mnist_added_oneshot_acc']
     ]
+    names = [x[1] for x in all_names]
+    exp_names = [x[0] for x in all_names]
     terms = []
     for name in names:
         fn = os.path.join('outputs', name, name + '.txt')
         terms.append(load(fn))
     matrix = np.asarray(terms)
-    matrix = np.transpose(matrix, [2, 1, 0, 3])
 
-    type_names = ['Shared', 'Individual']
-    exp_names = ['DNN', 'CNN', 'ResNet', 'ViT', 'LSTM', 'Transformer']
-    exp_names = ['\\multicolumn{1}{c}{' + x + '}' for x in exp_names]
-    for i in range(len(matrix)):
-        output_result(matrix[i], type_names, exp_names)
-        print()
+    # dump(matrix, exp_names)
+    dump_vertical(matrix, exp_names)
 
 
 if __name__ == '__main__':
