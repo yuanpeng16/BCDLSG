848a157a36c26d5d469e87206e27f7d6b8b4a1aa
Yuanpeng Li <yuanpeng16@hotmail.com>
2022-08-06 21:31:42 +0800

    Use different percentage.

---
+++ scripts/partition-t_main/partition-t_main_cnn-10.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ $# = 5 ]; then
+  LOG_DIR=$1
+  N_COMMON_LAYERS=$2
+  N_SEPARATE_LAYERS=$3
+  RANDOM_SEED=$4
+  GPU_ID=$5
+else
+  echo log_dir, n_common_layers, n_separate_layers, random_seed, and gpu_id.
+  exit
+fi
+
+echo \
+  CUDA_VISIBLE_DEVICES="${GPU_ID}" \
+  python3 -u main.py \
+  --parameter_random_seed "${RANDOM_SEED}" \
+  --data_random_seed "${RANDOM_SEED}" \
+  --merge_type added \
+  --model_type cnn \
+  --evaluator_type partition-t \
+  --partition_threshold_percentage 10 \
+  --log_interval 100 \
+  --batch_size 512 \
+  --lr 0.001 \
+  --steps 5000 \
+  --n_hidden_nodes 64 \
+  --n_common_layers "${N_COMMON_LAYERS}" \
+  --n_separate_layers "${N_SEPARATE_LAYERS}" \
+  --dataset1 cifar10 \
+  --dataset2 fashion_mnist \
+  --label_split diagonal \
+  --log_dir "${LOG_DIR}"
