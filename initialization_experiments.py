6e02723bcad351593e55b16469a9f461d0d366e7
yuanpeng16 <yuanpeng16@gmail.com>
2024-06-11 18:04:14 +0800

    all experiments

--- initialization_experiments.py
+++ initialization_experiments.py
@@ -23,7 +23,6 @@ import numpy as np
 import numpy.random as npr
 import argparse
 
-import jax.numpy as jnp
 from jax import random
 from jax.example_libraries import stax
 from jax.example_libraries.stax import Dense, Tanh, Relu, Softplus, Sigmoid, \
@@ -32,35 +31,11 @@ from jax.scipy.special import logsumexp
 import keras.datasets.mnist
 
 
-def get_activation(name):
-    if name == 'tanh':
-        return Tanh
-    elif name == 'relu':
-        return Relu
-    elif name == 'softplus':
-        return Softplus
-    elif name == 'sigmoid':
-        return Sigmoid
-    elif name == 'elu':
-        return Elu
-    elif name == 'leakyrelu':
-        return LeakyRelu
-    elif name == 'selu':
-        return Selu
-    elif name == 'gelu':
-        return Gelu
-    elif name == 'linear':
-        return None
-    else:
-        assert False
-
-
 class Experiment(object):
-    def __init__(self, args):
+    def __init__(self, args, activation):
         self.depth = args.depth
         self.n_outputs = 10
 
-        activation = get_activation(args.activation)
         layers = []
         for _ in range(self.depth):
             layers.append(Dense(args.width))
@@ -89,21 +64,13 @@ class Experiment(object):
         return predictions
 
 
-def experiment(args):
-    (train_images, train_labels), (
-        test_images, test_labels) = keras.datasets.mnist.load_data()
-    train_images = np.reshape(train_images, [train_images.shape[0], -1])
-    test_images = np.reshape(test_images, [test_images.shape[0], -1])
-
-    train_images = train_images / 256 - 0.5
-    test_images = test_images / 256 - 0.5
-    random_images = np.random.rand(*test_images.shape) - 0.5
-
+def one_experiment(args, activation, images):
+    train_images, test_images, random_images = images
     train_acc_list = []
     test_acc_list = []
     random_acc_list = []
     for _ in range(args.repeats):
-        experiment = Experiment(args)
+        experiment = Experiment(args, activation)
         rng = random.PRNGKey(npr.randint(1000000))
         _, init_params = experiment.init_random_params(rng, (-1, 28 * 28))
         train_acc = experiment.accuracy(init_params, train_images)
@@ -116,8 +83,8 @@ def experiment(args):
     return lists
 
 
-def main(args):
-    lists = experiment(args)
+def one_type(args, activation, images):
+    lists = one_experiment(args, activation, images)
     names = ["Train", "Test", "Random"]
     for x, name in zip(lists, names):
         x = np.asarray(x)
@@ -129,6 +96,38 @@ def main(args):
         print(name, '\t', mean, std, y_mean, y_std)
 
 
+def main(args):
+    types = [
+        ['Tanh', Tanh],
+        ['Relu', Relu],
+        ['Softplus', Softplus],
+        ['Sigmoid', Sigmoid],
+        ['Elu', Elu],
+        ['LeakyRelu', LeakyRelu],
+        ['Selu', Selu],
+        ['Gelu', Gelu],
+        ['Linear', None]
+    ]
+    if args.activation is not None:
+        m = {x: y for x, y in types}
+        assert args.activation in m
+        types = [[args.activation, m[args.activation]]]
+
+    (train_images, train_labels), (
+        test_images, test_labels) = keras.datasets.mnist.load_data()
+    train_images = np.reshape(train_images, [train_images.shape[0], -1])
+    test_images = np.reshape(test_images, [test_images.shape[0], -1])
+
+    train_images = train_images / 256 - 0.5
+    test_images = test_images / 256 - 0.5
+    random_images = np.random.rand(*test_images.shape) - 0.5
+    images = train_images, test_images, random_images
+
+    for name, activation in types:
+        print(name)
+        one_type(args, activation, images)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--repeats', type=int, default=100,
@@ -137,6 +136,6 @@ if __name__ == '__main__':
                         help='Depth.')
     parser.add_argument('--width', type=int, default=128,
                         help='Depth.')
-    parser.add_argument('--activation', type=str, default='relu',
+    parser.add_argument('--activation', type=str, default=None,
                         help='Activation name.')
     main(parser.parse_args())
